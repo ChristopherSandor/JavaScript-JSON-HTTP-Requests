f781f4de4439704a4b5e6d90873745edd593b426
CJ.Sandor <71267879+123csandor@users.noreply.github.com>
2022-11-30 09:43:35 -0500

    Uploading all my files

---
+++ style.js
@@ -0,0 +1,33 @@
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
+        } else if(request.readyState === 4){
+            callback("could not fetch data", undefined);
+        }
+    });
+
+    request.open('GET', 'todos.json');
+    request.send();
+};
+
+console.log(1);
+console.log(2);
+
+
+getTodos((err, data) => {
+    console.log("call back fired");
+
+    if(err){
+    console.log(err);
+    } else {
+    console.log(data);
+    }
+
+});
+
+console.log(3);
+console.log(4);
